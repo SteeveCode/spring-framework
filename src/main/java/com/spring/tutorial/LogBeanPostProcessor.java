22f82a62b87508dba8608936584437c1ead7f7f6
steevecode <stevecode@yahoo.com>
2023-08-17 13:31:23 +0100

    Dependency Injection - @Required & How A BeanPostProcessor Works

---
+++ src/main/java/com/spring/tutorial/LogBeanPostProcessor.java
@@ -0,0 +1,19 @@
+package com.spring.tutorial;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LogBeanPostProcessor implements BeanPostProcessor {
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("PostProcessorBeforeInitialization: " + beanName);
+        return  bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("PostProcessorAfterInitialization: " + beanName);
+        return  bean;    }
+}
